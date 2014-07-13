f3c7dfe7c85367c0c821ae2deabe629506e6d95a
Ilyas Ronef <ilyas@divandesign.biz>
2014-07-13 15:23:17 +0600

    Version 2.0

--- ddGetDate.php
+++ ddGetDate.php
@@ -1,16 +1,16 @@
 <?php
-/**
+/** 
  * ddGetDate.php
- * @version 1.9 (2010-11-23)
+ * @version 2.0 (2012-01-23)
  * 
  * @desc Выводит дату по заданному формату.
  * 
- * @param date {integer | string} - Дата. Доступные значения: 'current' - текущая дата. По умолчанию: дата публикации, если нет даты публикации, дата создания документа.
+ * @param date {integer | string} - Дата. Доступные значения: 'now' - текущая дата. По умолчанию: дата публикации, если нет даты публикации, дата создания документа.
  * @param format {string} - Формат, по которому выводить дату. По умолчанию: 'd.m.y'.
  * @param monthToStr {0; 1} - Отображать ли месяц строкой (января, февраля, марта и т.д.), в этом случае месяц в строке format должен быть задан как 'month'. По умолчанию: 0.
  * @param shortFormat {string} - Если задан короткий формат, то выводит дату относительно текущей, в этом случае дата в строке shotFormat должна быть задана как 'short'. По умолчанию: ''.
  * 
- * @copyright 2010, DivanDesign
+ * @copyright 2012, DivanDesign
  * http://www.DivanDesign.biz
  */
 
@@ -19,31 +19,36 @@ $monthToStr = ($monthToStr == '1') ? true : false;
 
 if (!isset($date)){
 	$date = ($modx->documentObject['pub_date']) ? $modx->documentObject['pub_date'] : $modx->documentObject['createdon'];
-}else if ($date == 'current'){
-	$date = time();
 }
 
-//Если задан короткий формат и совпадает год с месяцем, то пытаемся его вывести
-if (isset($shortFormat)){
-	//Если разница времени меньше чем в один день, то добавляем "Сегодня"
-	if (time() - date($date) < 86400){
-		return date(str_replace('short', 'Сегодня', $shortFormat), $date);
-	//Вчера
-	}else if (time() - date($date) < 172800){
-		return date(str_replace('short', 'Вчера', $shortFormat), $date);
-	//Позавчера
-	}else if (time() - date($date) < 259200){
-		return date(str_replace('short', 'Позавчера', $shortFormat), $date);
+//Если дата не является Unix-меткой
+if (!is_numeric($date)){
+	$date = strtotime($date);
+}
+
+if ($date){
+	//Если задан короткий формат и совпадает год с месяцем, то пытаемся его вывести
+	if (isset($shortFormat)){
+		//Если разница времени меньше чем в один день, то добавляем "Сегодня"
+		if (time() - date($date) < 86400){
+			return date(str_replace('short', 'Сегодня', $shortFormat), $date);
+		//Вчера
+		}else if (time() - date($date) < 172800){
+			return date(str_replace('short', 'Вчера', $shortFormat), $date);
+		//Позавчера
+		}else if (time() - date($date) < 259200){
+			return date(str_replace('short', 'Позавчера', $shortFormat), $date);
+		}
 	}
+	
+	if ($monthToStr){
+		$monthes = array('января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря');
+		$format = str_replace('month', '\m\o\n\t\h', $format);
+		$result = str_replace('month', $monthes[date('n',$date)-1], date($format, $date));
+	}else{
+		$result = date($format, $date);
+	}
+	
+	return $result;
 }
-
-if ($monthToStr){
-	$monthes = array('января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря');
-	$format = str_replace('month', '\m\o\n\t\h', $format);
-	$result = str_replace('month', $monthes[date('n',$date)-1], date($format, $date));
-}else{
-	$result = date($format, $date);
-}
-
-return $result;
 ?>
