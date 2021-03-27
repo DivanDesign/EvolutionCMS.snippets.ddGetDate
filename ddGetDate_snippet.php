0772e5fb6166bf4297f29f7a049b0fd0381da86d
Ilyas Ronef <ilyas@DivanDesign.ru>
2021-03-27 12:12:43 +0300

    * Minor changes.

--- ddGetDate_snippet.php
+++ ddGetDate_snippet.php
@@ -1,20 +1,27 @@
 <?php
 /** 
- * ddGetDate.php
+ * ddGetDate
  * @version 2.1.2 (2014-07-13)
  * 
  * @see README.md
  * 
- * @link http://code.divandesign.biz/modx/ddgetdate/2.1.2
+ * @link https://code.divandesign.biz/modx/ddgetdate
  * 
- * @copyright 2014, DivanDesign
- * http://www.DivanDesign.biz
+ * @copyright 2009–2014 DD Group {@link https://DivanDesign.biz }
  */
 
-$format = isset($format) ? $format : 'd.m.y';
+$format =
+	isset($format) ?
+	$format :
+	'd.m.y'
+;
 
 if (!isset($date)){
-	$date = ($modx->documentObject['pub_date']) ? $modx->documentObject['pub_date'] : $modx->documentObject['createdon'];
+	$date =
+		$modx->documentObject['pub_date'] ?
+		$modx->documentObject['pub_date'] :
+		$modx->documentObject['createdon']
+	;
 }
 
 //Если дата не является Unix-меткой
@@ -23,33 +30,133 @@ if (!is_numeric($date)){
 }
 
 if ($date){
-	if (isset($lang) && $lang == 'en'){
-		$short = array('Day before yesterday', 'Yesterday', 'Today');
-		$monthes = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+	if (
+		isset($lang) &&
+		$lang == 'en'
+	){
+		$short = array(
+			'Day before yesterday',
+			'Yesterday',
+			'Today'
+		);
+		$monthes = array(
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December'
+		);
 	}else{
-		$short = array('Позавчера', 'Вчера', 'Сегодня');
-		$monthes = array('января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря');
+		$short = array(
+			'Позавчера',
+			'Вчера',
+			'Сегодня'
+		);
+		$monthes = array(
+			'января',
+			'февраля',
+			'марта',
+			'апреля',
+			'мая',
+			'июня',
+			'июля',
+			'августа',
+			'сентября',
+			'октября',
+			'ноября',
+			'декабря'
+		);
 	}
 	
 	//Если задан короткий формат и совпадает год с месяцем, то пытаемся его вывести
 	if (isset($shortFormat)){
 		//Если разница времени меньше чем в один день, то добавляем "Сегодня"
-		if (time() - date($date) < 86400){
-			return date(str_replace('short', $short[2], $shortFormat), $date);
+		if (
+			(
+				time() -
+				date($date)
+			) <
+			86400
+		){
+			return date(
+				str_replace(
+					'short',
+					$short[2],
+					$shortFormat
+				),
+				$date
+			);
 		//Вчера
-		}else if (time() - date($date) < 172800){
-			return date(str_replace('short', $short[1], $shortFormat), $date);
+		}else if (
+			(
+				time() -
+				date($date)
+			) <
+			172800
+		){
+			return date(
+				str_replace(
+					'short',
+					$short[1],
+					$shortFormat
+				),
+				$date
+			);
 		//Позавчера
-		}else if (time() - date($date) < 259200){
-			return date(str_replace('short', $short[0], $shortFormat), $date);
+		}else if (
+			(
+				time() -
+				date($date)
+			) <
+			259200
+		){
+			return date(
+				str_replace(
+					'short',
+					$short[0],
+					$shortFormat
+				),
+				$date
+			);
 		}
 	}
 	
-	if (isset($monthToStr) && $monthToStr == '1'){
-		$format = str_replace('month', '\m\o\n\t\h', $format);
-		$result = str_replace('month', $monthes[date('n',$date)-1], date($format, $date));
+	if (
+		isset($monthToStr) &&
+		$monthToStr == '1'
+	){
+		$format = str_replace(
+			'month',
+			'\m\o\n\t\h',
+			$format
+		);
+		
+		$result = str_replace(
+			'month',
+			$monthes[
+				date(
+					'n',
+					$date
+				) -
+				1
+			],
+			date(
+				$format,
+				$date
+			)
+		);
 	}else{
-		$result = date($format, $date);
+		$result = date(
+			$format,
+			$date
+		);
 	}
 	
 	return $result;
